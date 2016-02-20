991ab73721255c260ae89b503d59e3f9a09dbe8b
adolfo portilla <adolfoportilla@adolfo.local>
2016-02-20 14:55:51 -0600

    Initial Commit

---
+++ Twitter2Tests/Twitter2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Twitter2Tests.swift
+//  Twitter2Tests
+//
+//  Created by adolfo portilla on 2/20/16.
+//  Copyright © 2016 Adolfo Portilla. All rights reserved.
+//
+
+import XCTest
+@testable import Twitter2
+
+class Twitter2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
